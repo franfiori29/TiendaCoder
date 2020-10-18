e86acc6c0429b8561be31a9d2ab33c2d7f6374df
franfiori29 <franfiori29@gmail.com>
2020-10-18 19:46:38 -0300

    agregado componente navbar y estilos

---
+++ src/components/NavBar.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+function NavBar() {
+  return (
+    <header className="menu">
+      <div className="logo">El Logo</div>
+      <ul className="nav-links">
+        <li>LINK 1</li>
+        <li>LINK 2</li>
+        <li>LINK 3</li>
+      </ul>
+    </header>
+  );
+}
+
+export default NavBar;
