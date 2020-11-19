312e0da9e2588f68c743de45dcde68862d50dbf3
franfiori29 <franfiori@gmail.com>
2020-11-18 22:05:51 -0300

    desafio 10

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -1,23 +1,33 @@
 import React from 'react';
 import { useCartContext } from '../../context/CartContext';
 import CartItem from './CartItem';
+import { Link } from 'react-router-dom';
 
 export default function Cart() {
     const { cart, clear } = useCartContext();
+    const totalPrice = cart.reduce((acc, item) => { return acc + (item.quantity * item.item.price) }, 0)
     return (
         <>
-            <div style={{
+            { cart.length && <div style={{
                 textAlign: 'center',
                 margin: 'auto',
                 marginTop: '10px',
                 fontSize: '30px',
                 borderBottom: '1px solid black',
                 width: '150px'
-            }} onClick={clear}><a href='#'>VACIAR</a></div>
+            }} onClick={clear}><a href='#'>VACIAR</a></div>}
             <div style={{ display: 'flex', flexWrap: 'wrap' }}>
                 {cart.length ? cart.map(elem => <CartItem item={elem.item} quantity={elem.quantity} />) :
-                    <h1 style={{ textAlign: 'center', width: '100%', margin: '20px' }}>Sin items...</h1>}
+                    <h1 style={{ textAlign: 'center', width: '100%', margin: '20px' }}>
+                        <Link to='/'>
+                            Sin items... <br />
+                            <button> Volver al menú principal</button>
+                        </Link>
+                    </h1>
+                }
             </div>
+            {totalPrice && <h2 style={{ textAlign: 'center', marginTop: '10px' }}>Total a pagar: {totalPrice}</h2>}
+
         </>
     )
 }
--- src/components/CartWidget/CartWidget.js
+++ src/components/CartWidget/CartWidget.js
@@ -4,10 +4,14 @@ import { useCartContext } from '../../context/CartContext';
 
 export default function CartWidget() {
     const { cart } = useCartContext();
+    const cartLength = cart.reduce((acc, item) => { return acc + item.quantity }, 0);
+
     return (
-        <div id="carrito">
-            <img style={{ width: 40 }} src={logo} alt='' />
-            {' ' + cart.length}
-        </div>
+        <div id="carrito" >
+            {cartLength != 0 && <div>
+                <img style={{ width: 40 }} src={logo} alt='' />
+                {' ' + cartLength}
+            </div>}
+        </div >
     )
 }
